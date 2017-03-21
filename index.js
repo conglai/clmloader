f491ade3d12c784a13111ee4c122c39da0f1c3b1
ykan <nameyukan@gmail.com>
2017-03-21 14:27:41 +0800

    增加了可以加载目录中的文件的功能

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 'use strict';
 const fs = require('fs');
 const co = require('co');
+const path = require('path');
 //# 加载目录下的模块
 // * args.path(*): 目录路径
 // * args.deps(*): 依赖的数组
 // * args.pre: map中的前缀
 // * args.defaultFile: 默认加载的文件名
 // * args.attach: 附加上的配置
+// * args.shouldLoadFile: 选择加载目录还是文件
 // return Promise.resolve(Map);
 module.exports = function(args) {
   let files = fs.readdirSync(args.path);
-  args.defaultFile = args.defaultFile || 'index';
+  let defaultFile = args.defaultFile || 'index';
+  let shouldLoadFile = !!args.shouldLoadFile;
+  let ctx = args.ctx || {};
   return co(function*(){
     let map = {};
     let keys = [];
@@ -21,17 +25,22 @@ module.exports = function(args) {
       let file = files[i];
       let fullPath = `${args.path}/${file}`;
       let stat = fs.lstatSync(fullPath);
-      if(stat.isDirectory()) {
-        let filePath = `${fullPath}/${args.defaultFile}.js`;
+      let filePath, modName;
+      if(!shouldLoadFile && stat.isDirectory()) {
+        filePath = `${fullPath}/${defaultFile}.js`;
+        modName = file;
+      } else if(shouldLoadFile && stat.isFile() && path.extname(file) === '.js') {
+        filePath = fullPath;
+        modName = file.replace(/\.js$/, '');
+      }
+      if(filePath) {
         let modFunc = require(filePath);
-        let modObj = yield modFunc.apply({}, args.deps);
-        let name = modObj.name || file;
-        modObj.path = fullPath;
-        modObj.name = name;
+        let modObj = yield modFunc.apply(ctx, args.deps);
+        modObj.name = modName;
         keys.forEach(key => {
           modObj[key] = modObj[key] || args.attach[key];
         });
-        map[name] = modObj;
+        map[modName] = modObj;
       }
     }
     return map;
--- tests/main.test.js
+++ tests/main.test.js
@@ -16,7 +16,8 @@ describe('加载模块测试', () => {
       dep2.test.should.be.equal(4);
     });
   });
-  it('修改默认地址', () => {
+
+  it('修改默认模块名', () => {
     return co(function*(){
       let dep1 = { test: 0 };
       let dep2 = { test: 0 };
@@ -32,9 +33,25 @@ describe('加载模块测试', () => {
       map['dir1'].common.should.be.equal('xx');
       map['dir2'].common.should.be.equal('xxx');
       map['dir2'].name.should.be.equal('dir2');
-      map['dir2'].path.should.be.equal(__dirname + '/examples-1/dir2');
       dep1.test.should.be.equal(2);
       dep2.test.should.be.equal(4);
+    });
+  });
+
+  it('加载文件模块', () => {
+    return co(function*(){
+      let dep1 = { test: 0 };
+      let dep2 = { test: 0 };
+      let map = yield loadMoudles({
+        path: __dirname + '/examples-2',
+        deps: [dep1, dep2],
+        defaultFile: 'router',
+        shouldLoadFile: true,
+        attach: {
+          common: 'xx'
+        }
+      });
+      map.should.have.keys('router', 'router-12');
     });
   });
 });
